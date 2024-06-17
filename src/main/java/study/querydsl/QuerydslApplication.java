8235bfa59234ee5e51e117734bbda8dec87c26eb
memoryDev <memorydev2024@gmail.com>
2024-06-17 16:42:46 +0900

    스프링 데이터 JPA와 Querydsl

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,9 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -9,5 +11,4 @@ public class QuerydslApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
-
 }
